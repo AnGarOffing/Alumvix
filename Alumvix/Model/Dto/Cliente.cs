2976a4ed8c0d8b05e812e490c87ea69be6dc6bd7
Offingadev <ag.andresgarcia.x@gmail.com>
2021-12-22 21:42:11 -0500

    Se crea la arquitectura MVC

---
+++ Alumvix/Model/Dto/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumvix.Model.Dto
+{
+    internal class Cliente
+    {
+    }
+}
