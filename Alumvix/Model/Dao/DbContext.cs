cb14ce68399e35e8334065f6bf4840c5cdd82c84
Offingadev <ag.andresgarcia.x@gmail.com>
2021-12-22 23:50:05 -0500

    Se crean las clases: Cliente con sus atributos y propiedades,ClienteDao vacia y DbContext con cadena de conexion a la BD

--- Alumvix/Model/Dao/DbContext.cs
+++ Alumvix/Model/Dao/DbContext.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Alumvix.Model.Dao
 {
-    internal class DbContext
+    public class DbContext
     {
+        protected SqlConnection _connection = new SqlConnection(
+            "Server=DESKTOP-N7D39RE\\SQLEXPRESS;DataBase=ALUMVIX;Integrated Security=true");
     }
 }
--- Alumvix/Model/Dto/Cliente.cs
+++ Alumvix/Model/Dto/Cliente.cs
@@ -8,5 +8,24 @@ namespace Alumvix.Model.Dto
 {
     internal class Cliente
     {
+        #region atributos
+        int _id;
+        string _identificacionCliente;
+        string _nombreCliente;
+        string _correoElectronicoCliente;
+        string _celularCliente;
+        string _telefonoFijoCliente;
+        string _direccionCliente;
+
+        public int Id { get => _id; set => _id = value; }
+        public string IdentificacionCliente { get => _identificacionCliente; set => _identificacionCliente = value; }
+        public string NombreCliente { get => _nombreCliente; set => _nombreCliente = value; }
+        public string CorreoElectronicoCliente { get => _correoElectronicoCliente; set => _correoElectronicoCliente = value; }
+        public string CelularCliente { get => _celularCliente; set => _celularCliente = value; }
+        public string TelefonoFijoCliente { get => _telefonoFijoCliente; set => _telefonoFijoCliente = value; }
+        public string DireccionCliente { get => _direccionCliente; set => _direccionCliente = value; }
+        #endregion
+
+
     }
 }
