fe8c58cbca6d3364e2a150fd22f6eb04e470d2ea
Offingadev <ag.andresgarcia.x@gmail.com>
2022-01-13 21:40:22 -0500

    Se crea formulario para ingresar un nuevo contrato

---
+++ Alumvix/View/Contrato/IngresoContratoView.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Alumvix.View.Contrato
+{
+    partial class IngresoContratoView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlIngresarContrato = new System.Windows.Forms.Panel();
+            this.cbIngresarTipoFactura = new System.Windows.Forms.ComboBox();
+            this.lblIngresarTipoFactura = new System.Windows.Forms.Label();
+            this.dtpFechaInicioContrato = new System.Windows.Forms.DateTimePicker();
+            this.btnGuardarNuevoContrato = new System.Windows.Forms.Button();
+            this.txtIngresarValorGasto = new System.Windows.Forms.TextBox();
+            this.lvlValorIngresoGasto = new System.Windows.Forms.Label();
+            this.lblIngresarFechaInicioContrato = new System.Windows.Forms.Label();
+            this.lblTituloIngresarContrato = new System.Windows.Forms.Label();
+            this.lblIngresarFechaFinalContrato = new System.Windows.Forms.Label();
+            this.dtpFechaTerminacionContrato = new System.Windows.Forms.DateTimePicker();
+            this.pnlIngresarContrato.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlIngresarContrato
+            // 
+            this.pnlIngresarContrato.Controls.Add(this.dtpFechaTerminacionContrato);
+            this.pnlIngresarContrato.Controls.Add(this.lblIngresarFechaFinalContrato);
+            this.pnlIngresarContrato.Controls.Add(this.cbIngresarTipoFactura);
+            this.pnlIngresarContrato.Controls.Add(this.lblIngresarTipoFactura);
+            this.pnlIngresarContrato.Controls.Add(this.dtpFechaInicioContrato);
+            this.pnlIngresarContrato.Controls.Add(this.btnGuardarNuevoContrato);
+            this.pnlIngresarContrato.Controls.Add(this.txtIngresarValorGasto);
+            this.pnlIngresarContrato.Controls.Add(this.lvlValorIngresoGasto);
+            this.pnlIngresarContrato.Controls.Add(this.lblIngresarFechaInicioContrato);
+            this.pnlIngresarContrato.Location = new System.Drawing.Point(12, 61);
+            this.pnlIngresarContrato.Name = "pnlIngresarContrato";
+            this.pnlIngresarContrato.Size = new System.Drawing.Size(488, 217);
+            this.pnlIngresarContrato.TabIndex = 41;
+            // 
+            // cbIngresarTipoFactura
+            // 
+            this.cbIngresarTipoFactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbIngresarTipoFactura.FormattingEnabled = true;
+            this.cbIngresarTipoFactura.Location = new System.Drawing.Point(177, 74);
+            this.cbIngresarTipoFactura.Name = "cbIngresarTipoFactura";
+            this.cbIngresarTipoFactura.Size = new System.Drawing.Size(218, 28);
+            this.cbIngresarTipoFactura.TabIndex = 43;
+            // 
+            // lblIngresarTipoFactura
+            // 
+            this.lblIngresarTipoFactura.AutoSize = true;
+            this.lblIngresarTipoFactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngresarTipoFactura.Location = new System.Drawing.Point(15, 74);
+            this.lblIngresarTipoFactura.Name = "lblIngresarTipoFactura";
+            this.lblIngresarTipoFactura.Size = new System.Drawing.Size(156, 24);
+            this.lblIngresarTipoFactura.TabIndex = 42;
+            this.lblIngresarTipoFactura.Text = "Tipo de factura:";
+            // 
+            // dtpFechaInicioContrato
+            // 
+            this.dtpFechaInicioContrato.CustomFormat = "yyyy/MM/dd";
+            this.dtpFechaInicioContrato.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFechaInicioContrato.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFechaInicioContrato.Location = new System.Drawing.Point(182, 121);
+            this.dtpFechaInicioContrato.Name = "dtpFechaInicioContrato";
+            this.dtpFechaInicioContrato.Size = new System.Drawing.Size(120, 26);
+            this.dtpFechaInicioContrato.TabIndex = 40;
+            // 
+            // btnGuardarNuevoContrato
+            // 
+            this.btnGuardarNuevoContrato.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarNuevoContrato.Location = new System.Drawing.Point(392, 165);
+            this.btnGuardarNuevoContrato.Name = "btnGuardarNuevoContrato";
+            this.btnGuardarNuevoContrato.Size = new System.Drawing.Size(83, 33);
+            this.btnGuardarNuevoContrato.TabIndex = 39;
+            this.btnGuardarNuevoContrato.Text = "Guardar";
+            this.btnGuardarNuevoContrato.UseVisualStyleBackColor = true;
+            // 
+            // txtIngresarValorGasto
+            // 
+            this.txtIngresarValorGasto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIngresarValorGasto.Location = new System.Drawing.Point(177, 30);
+            this.txtIngresarValorGasto.Name = "txtIngresarValorGasto";
+            this.txtIngresarValorGasto.Size = new System.Drawing.Size(100, 26);
+            this.txtIngresarValorGasto.TabIndex = 34;
+            // 
+            // lvlValorIngresoGasto
+            // 
+            this.lvlValorIngresoGasto.AutoSize = true;
+            this.lvlValorIngresoGasto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlValorIngresoGasto.Location = new System.Drawing.Point(106, 30);
+            this.lvlValorIngresoGasto.Name = "lvlValorIngresoGasto";
+            this.lvlValorIngresoGasto.Size = new System.Drawing.Size(65, 24);
+            this.lvlValorIngresoGasto.TabIndex = 33;
+            this.lvlValorIngresoGasto.Text = "Valor:";
+            // 
+            // lblIngresarFechaInicioContrato
+            // 
+            this.lblIngresarFechaInicioContrato.AutoSize = true;
+            this.lblIngresarFechaInicioContrato.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngresarFechaInicioContrato.Location = new System.Drawing.Point(15, 121);
+            this.lblIngresarFechaInicioContrato.Name = "lblIngresarFechaInicioContrato";
+            this.lblIngresarFechaInicioContrato.Size = new System.Drawing.Size(161, 24);
+            this.lblIngresarFechaInicioContrato.TabIndex = 35;
+            this.lblIngresarFechaInicioContrato.Text = "Fecha de inicio:";
+            // 
+            // lblTituloIngresarContrato
+            // 
+            this.lblTituloIngresarContrato.AutoSize = true;
+            this.lblTituloIngresarContrato.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloIngresarContrato.Location = new System.Drawing.Point(7, 20);
+            this.lblTituloIngresarContrato.Name = "lblTituloIngresarContrato";
+            this.lblTituloIngresarContrato.Size = new System.Drawing.Size(215, 29);
+            this.lblTituloIngresarContrato.TabIndex = 40;
+            this.lblTituloIngresarContrato.Text = "Ingresar Contrato";
+            // 
+            // lblIngresarFechaFinalContrato
+            // 
+            this.lblIngresarFechaFinalContrato.AutoSize = true;
+            this.lblIngresarFechaFinalContrato.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngresarFechaFinalContrato.Location = new System.Drawing.Point(15, 168);
+            this.lblIngresarFechaFinalContrato.Name = "lblIngresarFechaFinalContrato";
+            this.lblIngresarFechaFinalContrato.Size = new System.Drawing.Size(220, 24);
+            this.lblIngresarFechaFinalContrato.TabIndex = 48;
+            this.lblIngresarFechaFinalContrato.Text = "Fecha de terminacion:";
+            // 
+            // dtpFechaTerminacionContrato
+            // 
+            this.dtpFechaTerminacionContrato.CustomFormat = "yyyy/MM/dd";
+            this.dtpFechaTerminacionContrato.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFechaTerminacionContrato.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFechaTerminacionContrato.Location = new System.Drawing.Point(241, 166);
+            this.dtpFechaTerminacionContrato.Name = "dtpFechaTerminacionContrato";
+            this.dtpFechaTerminacionContrato.Size = new System.Drawing.Size(120, 26);
+            this.dtpFechaTerminacionContrato.TabIndex = 49;
+            // 
+            // IngresoContratoView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 290);
+            this.Controls.Add(this.pnlIngresarContrato);
+            this.Controls.Add(this.lblTituloIngresarContrato);
+            this.Name = "IngresoContratoView";
+            this.Text = "IngresoContratoView";
+            this.pnlIngresarContrato.ResumeLayout(false);
+            this.pnlIngresarContrato.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlIngresarContrato;
+        public System.Windows.Forms.ComboBox cbIngresarTipoFactura;
+        private System.Windows.Forms.Label lblIngresarTipoFactura;
+        public System.Windows.Forms.DateTimePicker dtpFechaInicioContrato;
+        public System.Windows.Forms.Button btnGuardarNuevoContrato;
+        public System.Windows.Forms.TextBox txtIngresarValorGasto;
+        private System.Windows.Forms.Label lvlValorIngresoGasto;
+        private System.Windows.Forms.Label lblIngresarFechaInicioContrato;
+        public System.Windows.Forms.Label lblTituloIngresarContrato;
+        public System.Windows.Forms.DateTimePicker dtpFechaTerminacionContrato;
+        private System.Windows.Forms.Label lblIngresarFechaFinalContrato;
+    }
+}
